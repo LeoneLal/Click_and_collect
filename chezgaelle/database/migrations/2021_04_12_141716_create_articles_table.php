44ec5aaec15726059655e6e06ead8d75513003a1
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-04-12 16:46:23 +0200

    Add migration files

--- chezgaelle/database/migrations/2021_04_12_141716_create_articles_table.php
+++ chezgaelle/database/migrations/2021_04_12_141716_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticleTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateArticleTable extends Migration
      */
     public function up()
     {
-        Schema::create('article', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('body');
@@ -30,6 +30,6 @@ class CreateArticleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('article');
+        Schema::dropIfExists('articles');
     }
 }
