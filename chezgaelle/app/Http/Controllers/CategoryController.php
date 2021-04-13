7ab5ec3fc9740eed251acc32b51e01d4400ce301
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-04-13 11:20:39 +0200

    Add delete category function in controller

--- chezgaelle/app/Http/Controllers/CategoryController.php
+++ chezgaelle/app/Http/Controllers/CategoryController.php
@@ -85,6 +85,8 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::where('id', $id)->first();
+        $category->delete();
+        return redirect()->route('categories.index');
     }
 }
