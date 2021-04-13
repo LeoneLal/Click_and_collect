72dbcd4c94bcbb9f5db8d5156f167f66c74c54a5
Auriane Labille <auriane.labille@gmail.com>
2021-04-13 16:23:58 +0200

    Update Products Routes

--- chezgaelle/resources/views/products/create.blade.php
+++ chezgaelle/resources/views/products/create.blade.php
@@ -13,7 +13,7 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ url('products/create') }}">
+        <form method="POST" action="{{ route('product.create') }}">
             @csrf
 
             <!-- Name -->
