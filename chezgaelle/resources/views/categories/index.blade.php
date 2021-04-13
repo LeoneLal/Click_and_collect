32f17f68fd4d7e3934393f92ed10a51fda0e304b
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-04-13 11:21:44 +0200

    Add list of all categories

--- chezgaelle/resources/views/categories/index.blade.php
+++ chezgaelle/resources/views/categories/index.blade.php
@@ -10,10 +10,24 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <a href="{{ url('/categories/create') }}"><button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                        Ajouter une catégorie
+                        Add a category
                     </button></a>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @foreach($categories as $category)
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <p>{{ $category->name }}</p>
+                    
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
 </x-app-layout>
