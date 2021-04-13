ff77b28243f1415864eca86f582cf986976ba6ee
Auriane Labille <auriane.labille@gmail.com>
2021-04-13 10:47:45 +0200

    Create Product Routes

--- chezgaelle/routes/web.php
+++ chezgaelle/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 
 /*
@@ -31,5 +32,11 @@ Route::get('/category/{id}', [CategoryController::class, 'show'])->name('show');
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('create');
 Route::post('/categories/create', [CategoryController::class, 'store'])->name('store');;
 Route::get('/category/{id}/update', [CategoryController::class, 'update'])->name('update');
+
+Route::get('/products', [ProductController::class, 'index'])->name('index');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('show');
+Route::get('/products/create', [ProductController::class, 'create'])->name('create');
+Route::post('/products/create', [ProductController::class, 'store'])->name('store');;
+Route::get('/product/{id}/update', [ProductController::class, 'update'])->name('update');
 
 require __DIR__.'/auth.php';
