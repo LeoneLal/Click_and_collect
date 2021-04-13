e117955c053ceb5f686161bc85b50e5b76ad82df
Auriane Labille <auriane.labille@gmail.com>
2021-04-13 15:27:41 +0200

    Add Products Index

--- chezgaelle/resources/views/products/index.blade.php
+++ chezgaelle/resources/views/products/index.blade.php
@@ -18,4 +18,23 @@
             </div>
         </div>
     </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @foreach($products as $product)
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <img href="" />
+                    <p>{{ $product->name }}</p>
+                    <p>{{ $product->price }} €</p>
+                    @if($product->stock > 0)
+                        <p class="text-green-400">In stock, only {{ $product->stock }} left</p>
+                    @else
+                        <p class="text-red-500">Sold out</p>
+                    @endif
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </x-app-layout>
