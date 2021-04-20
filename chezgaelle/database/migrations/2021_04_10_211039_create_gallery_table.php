a1f042575bf1ebacbbd2fa91ac2cdd6292e994a2
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-04-20 14:12:21 +0200

    Update 2021_04_10_211039_create_gallery_table.php

--- chezgaelle/database/migrations/2021_04_10_211039_create_gallery_table.php
+++ chezgaelle/database/migrations/2021_04_10_211039_create_gallery_table.php
@@ -15,7 +15,7 @@ class CreateGalleryTable extends Migration
     {
         Schema::create('gallery', function (Blueprint $table) {
             $table->id();
-            $table->string('file_path');
+            $table->string('picture_slug');
             $table->string('description');
             $table->timestamps();
         });
