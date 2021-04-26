c4c714e79dfb6c4b027b7e9ab82b1bc4eed684c5
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-04-26 08:08:49 +0200

    Create ProductController.php

---
+++ chezgaelle/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+}
