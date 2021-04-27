298f75183d468b7fba5bf710b0797bb5760ea334
Auriane Labille <auriane.labille@gmail.com>
2021-04-27 14:08:44 +0200

    Update show.blade.php

--- chezgaelle/resources/views/articles/show.blade.php
+++ chezgaelle/resources/views/articles/show.blade.php
@@ -12,8 +12,10 @@
                     <img class="m-auto" src="{{ URL::to('/') }}/images/articles/{{ $article->picture_path }}">
                     <div class="show-details">
                         <p>{{ $article->title }}</p>
-                        <p class="text-xs">Auteur : {{ $article->author }}</p>
-                        <p class="text-xs">Mis à jour le {{ date_format($article->updated_at, "d/m/Y") }}</p>
+                        <div class="flex justify-between">
+                            <p class="text-xs">Auteur : {{ $article->author }}</p>
+                            <p class="text-xs">Mis à jour le {{ date_format($article->updated_at, "d/m/Y") }}</p>
+                        </div>
                         <p>{{ $article->body }}</p>
                     </div>
                 </div>
