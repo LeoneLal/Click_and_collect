efc04dfd1db60d502d96d3745c31f351e4bee090
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-05-11 11:44:00 +0200

    Add order api route

--- chezgaelle/routes/api.php
+++ chezgaelle/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderLineController;
 use App\Http\Controllers\Api\AuthController;
 
 /*
@@ -18,6 +20,8 @@ use App\Http\Controllers\Api\AuthController;
 */
 Route::apiResource('products', ProductController::class);
 Route::apiResource('category', CategoryController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('orderlines', OrderLineController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
