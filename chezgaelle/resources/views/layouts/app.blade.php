bc7c22c7c9b7857afc8d9d8061be3a74fabd2449
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-05-18 10:31:44 +0200

    Add images

--- chezgaelle/resources/views/layouts/app.blade.php
+++ chezgaelle/resources/views/layouts/app.blade.php
@@ -13,12 +13,12 @@
         <!-- Styles -->
         <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}"> -->
-        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
         <!-- Scripts -->
         <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
-        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
